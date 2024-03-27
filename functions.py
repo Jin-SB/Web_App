c551acd3abadb6d3c73ac7a300ec4428fbf032be
Jin Bansal <jin_sb@hotmail.com>
2024-03-27 21:53:53 +0000

    new file:   functions.py 	new file:   todos.txt 	new file:   web.py

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "/Users/jinbansal/Code/Python_Mega_Course/Projects/ToDo_App/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ 
+    Read a text file and return 
+    the list of to-do items 
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()  
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write the to-do items list to the text file
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
